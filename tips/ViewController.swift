6bd8cf2a87ca79a85dc702235e8c5b3d6b0cfcea
inajnaa <anjanib@gmail.com>
2016-01-12 22:05:47 -0800

    added split

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -21,6 +21,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalText: UILabel!
     @IBOutlet weak var tipIcon: UIImageView!
     @IBOutlet weak var tipText: UILabel!
+    @IBOutlet weak var splitTwo: UITextField!
+    @IBOutlet weak var splitThree: UITextField!
+    @IBOutlet weak var splitFour: UITextField!
+    @IBOutlet weak var splitBG: UIView!
+    @IBOutlet weak var splitTitle: UITextField!
+    @IBOutlet weak var twoLabel: UITextField!
+    @IBOutlet weak var threeLabel: UITextField!
+    @IBOutlet weak var fourLabel: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,8 +42,21 @@ class ViewController: UIViewController {
         self.tipLabel.alpha = 0
         self.totalLabel.alpha = 0
         self.tipDivider.alpha = 0
+        self.tipControl.alpha = 0
+        self.splitBG.alpha = 0
+        self.splitTitle.alpha = 0
+        self.twoLabel.alpha = 0
+        self.threeLabel.alpha = 0
+        self.fourLabel.alpha = 0
+        self.splitTwo.alpha = 0
+        self.splitThree.alpha = 0
+        self.splitFour.alpha = 0
+        
         tipLabel.text = "$0.00"
         totalLabel.text = "0.00"
+        splitTwo.text = "0.00"
+        splitThree.text = "0.00"
+        splitFour.text = "0.00"
     }
     
     override func didReceiveMemoryWarning() {
@@ -52,6 +73,13 @@ class ViewController: UIViewController {
         })
     }
 
+    @IBAction func onTapPrice(sender: AnyObject) {
+        
+        UIView.animateWithDuration(2.0, animations: {
+            self.tipControl.alpha = 1.0
+            })
+
+    }
     
     @IBAction func onEditingChange(sender: AnyObject) {
         
@@ -61,6 +89,14 @@ class ViewController: UIViewController {
             self.tipLabel.alpha = 1.0
             self.totalLabel.alpha = 1.0
             self.tipDivider.alpha = 1.0
+            self.splitBG.alpha = 1.0
+            self.splitTitle.alpha = 1.0
+            self.twoLabel.alpha = 1.0
+            self.threeLabel.alpha = 1.0
+            self.fourLabel.alpha = 1.0
+            self.splitTwo.alpha = 1.0
+            self.splitThree.alpha = 1.0
+            self.splitFour.alpha = 1.0
         })
         
         UIView.animateWithDuration(1.0, animations: {
@@ -71,13 +107,13 @@ class ViewController: UIViewController {
 
         UIView.animateWithDuration(1.0, animations: {
             var newCenter = self.dollarLabel.center
-            newCenter.y = 165
+            newCenter.y = 175
             self.dollarLabel.center = newCenter
         })
 
         UIView.animateWithDuration(1.0, animations: {
             var newCenter = self.billField.center
-            newCenter.y = 165
+            newCenter.y = 175
             self.billField.center = newCenter
         })
 
@@ -87,17 +123,28 @@ class ViewController: UIViewController {
         let billAmount = NSString(string: billField.text!).doubleValue
         let tip = billAmount * tipPercentage
         let total = billAmount + tip
+        
+        let twoWay = total/2
+        let threeWay = total/3
+        let fourWay = total/4
 
         tipLabel.text = "$\(tip)"
         totalLabel.text = "$\(total)"
+        splitTwo.text = "$\(twoWay)"
+        splitThree.text = "$\(threeWay)"
+        splitFour.text = "$\(fourWay)"
+        
         
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
+        splitTwo.text = String(format: "$%.2f", twoWay)
+        splitThree.text = String(format: "$%.2f", threeWay)
+        splitFour.text = String(format: "$%.2f", fourWay)
     }
 
     @IBAction func onTap(sender: AnyObject) {
         view.endEditing(true)
     }
-
+ 
 }
 
